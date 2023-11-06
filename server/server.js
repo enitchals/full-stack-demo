3a92dc1f602bf090b4aeeb4626eea4462311ed22
Ellen Nitchals <ellen.nitchals@gmail.com>
2023-11-06 12:50:20 -0800

    basic express server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express();
+
+const PORT = 3000;
+
+// body parsing middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// error handling middleware
+app.use((err, req, res, next) => {
+  res.status(err.status ?? 500).send(err.message ?? 'Error!');
+})
+
+// start the server
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
