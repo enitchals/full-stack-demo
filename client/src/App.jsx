5ec6aa56d7567dc9feef0152bd84c74d9dbf9821
Ellen Nitchals <ellen.nitchals@gmail.com>
2023-11-06 12:33:25 -0800

    react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
 import TaskForm from './components/TaskForm'
 import TaskList from './components/TaskList'
@@ -6,8 +7,16 @@ function App() {
 
   return (
     <>
-      <TaskForm/>
-      <TaskList/>
+      <h1>To Do List</h1>
+      <nav>
+        <Link to="/tasks">All Tasks</Link>
+        <br/>
+        <Link to="/new">Add Task</Link>
+      </nav>
+      <Routes>
+        <Route path="/tasks" element={<TaskList/>}/>
+        <Route path="/new" element={<TaskForm/>}/>
+      </Routes>
     </>
   )
 }
